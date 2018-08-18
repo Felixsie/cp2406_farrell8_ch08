d48503048308fe679b5dd7b8be2036b54d2d9c0a
Felixsie <31508904+Felixsie@users.noreply.github.com>
2018-08-18 02:52:04 -0700

    Finished Chapter 8

---
+++ Programming Excercises/Salesperson.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Lenovo on 8/17/2018.
+ */
+
+public class Salesperson
+{
+    private int id;
+    private double sales;
+    Salesperson(int idNum, double amt)
+    {
+        id = idNum;
+        sales = amt;
+    }
+    public int getId()
+    {
+        return id;
+    }
+    public double getSales()
+    {
+        return sales;
+    }
+    public void setId(int idNum)
+    {
+        id = idNum;
+    }
+    public void setSales(double amt)
+    {
+        sales = amt;
+    }
+}
